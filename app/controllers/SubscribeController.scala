20d0531aa522d27c491ca6b46491d1d16b9062df
Toby Sullivan <tobyjsullivan@gmail.com>
2016-12-23 15:40:57 -0800

    Save all registration info

--- app/controllers/SubscribeController.scala
+++ app/controllers/SubscribeController.scala
@@ -23,6 +23,16 @@ class SubscribeController @Inject() extends Controller {
     val oName = formData.get("name").map(_.head)
     val oEmail = formData.get("email").map(_.head)
     val oSize = formData.get("shirt-size").map(_.head)
+    val oSms = formData.get("sms-number").map(_.head)
+    val oAddressName = formData.get("address-name").map(_.head)
+    val oAddressLine1 = formData.get("address-line-1").map(_.head)
+    val oAddressLine2 = formData.get("address-line-2").map(_.head)
+    val oAddressCity = formData.get("address-city").map(_.head)
+    val oAddressProvince = formData.get("address-province").map(_.head)
+    val oAddressPostalCode = formData.get("address-postal-code").map(_.head)
+    val oAddressCountry = formData.get("address-country").map(_.head)
+    val oStripeToken = formData.get("stripeToken").map(_.head)
+    val oStripeEmail = formData.get("stripeEmail").map(_.head)
 
     if (oName.isEmpty) {
       Future.successful(BadRequest("Name must be provided"))
@@ -30,8 +40,33 @@ class SubscribeController @Inject() extends Controller {
       Future.successful(BadRequest("Email must be provided"))
     } else if (oSize.isEmpty) {
       Future.successful(BadRequest("Size must be provided"))
+    } else if (oAddressName.isEmpty) {
+      Future.successful(BadRequest("Address Name must be provided"))
+    } else if (oAddressLine1.isEmpty) {
+      Future.successful(BadRequest("Address Line 1 must be provided"))
+    } else if (oAddressCity.isEmpty) {
+      Future.successful(BadRequest("City must be provided"))
+    } else if (oAddressProvince.isEmpty) {
+      Future.successful(BadRequest("Province must be provided"))
+    } else if (oAddressPostalCode.isEmpty) {
+      Future.successful(BadRequest("Postal Code must be provided"))
+    } else if (oStripeToken.isEmpty) {
+      Future.successful(BadRequest("Payment must be provided"))
     } else {
-      val f = SubscriptionService.create(oName.get, oSize.get, oEmail.get)
+      val f = SubscriptionService.create(
+        name = oName.get,
+        size = oSize.get,
+        email = oEmail.get,
+        smsNumber = oSms,
+        addressName = oAddressName.get,
+        addressLine1 = oAddressLine1.get,
+        addressLine2 = oAddressLine2,
+        addressCity = oAddressCity.get,
+        addressProvince = oAddressProvince.get,
+        addressPostalCode = oAddressPostalCode.get,
+        addressCountry = oAddressCountry.getOrElse("CA"),
+        stripeToken = oStripeToken.get
+      )
 
       f.map { _ =>
         Ok(s"Subscription created: ${oName.getOrElse("Unknown")}")
--- app/services/SubscriptionService.scala
+++ app/services/SubscriptionService.scala
@@ -4,6 +4,8 @@ import java.util.UUID
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBuilder}
+import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.{DateTime, DateTimeZone}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -11,12 +13,40 @@ import scala.concurrent.{ExecutionContext, Future}
 object SubscriptionService {
   private val tableName = "tailored.monthly.subscriptions"
 
-  def create(name: String, size: String, email: String)(implicit executionContext: ExecutionContext): Future[Unit] = {
+  def create(
+              name: String,
+              size: String,
+              email: String,
+              smsNumber: Option[String],
+              addressName: String,
+              addressLine1: String,
+              addressLine2: Option[String],
+              addressCity: String,
+              addressProvince: String,
+              addressPostalCode: String,
+              addressCountry: String,
+              stripeToken: String
+            )(implicit executionContext: ExecutionContext): Future[Unit] = {
     val item: java.util.Map[String, AttributeValue] = new java.util.HashMap[String, AttributeValue]()
     item.put("ID", new AttributeValue(UUID.randomUUID().toString))
     item.put("Name", new AttributeValue(name))
     item.put("Email", new AttributeValue(email))
     item.put("Size", new AttributeValue(size))
+    if (smsNumber.isDefined) {
+      item.put("SMS", new AttributeValue(smsNumber.get))
+    }
+    item.put("AddressName", new AttributeValue(addressName))
+    item.put("AddressLine1", new AttributeValue(addressLine1))
+    item.put("AddressLine2", new AttributeValue(addressLine2.getOrElse("")))
+    item.put("AddressCity", new AttributeValue(addressCity))
+    item.put("AddressProvince", new AttributeValue(addressProvince))
+    item.put("AddressPostalCode", new AttributeValue(addressPostalCode))
+    item.put("AddressCountry", new AttributeValue(addressCountry))
+    item.put("StripeToken", new AttributeValue(stripeToken))
+
+    val signupDate = DateTime.now(DateTimeZone.UTC)
+    val fmt = ISODateTimeFormat.dateTime()
+    item.put("SignupDate", new AttributeValue(fmt.print(signupDate)))
 
     Future {
       dynamoDbClient.putItem(tableName, item)
